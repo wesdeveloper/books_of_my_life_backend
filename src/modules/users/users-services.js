5ef5a91329c3e5a120661eddcbc77ee0bf18e936
Marcos Santos <marcosadriano034@gmail.com>
2019-10-29 22:28:27 -0300

    refactore service getUserByid

--- src/modules/users/users-services.js
+++ src/modules/users/users-services.js
@@ -1,10 +1,6 @@
 const database = require('../database');
 
 const createUser = data => database.User.create(data);
-const getUserById = id => database.User.findOne({
-    where: {
-        id: id
-    }
-});
+const getUserById = id => database.User.findOne({ id });
 
 module.exports = { createUser, getUserById };
